515d017937a1b37f5018fb4345bb1c2461cc9f90
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-04-07 20:55:02 +0000

    Update progm1.py

--- progm1.py
+++ progm1.py
@@ -16,7 +16,21 @@ filesname = 'files/thing.names'
 
 with open(filesname,'rt') as f:
     filesname = f.read().rstrip('\n').split('\n')
-    print(filesname)
+    #print(filesname)
+    
+    x = "files/frozen_inference_graph.pb"
+    
+    y = "files/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+    
+    net = cv2.dnn_DetectionModel(x , y)
+    
+    net.setInputSize(320,230)
+    net.setInputMean((127.5,127.5,127.5))
+    net.setInputScale(1.0/127.5)
+    net.setInputSwapRB(True)
+    
+    
+    
 
 cv2.imshow("yassine windows",image)
 
